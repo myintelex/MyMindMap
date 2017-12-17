1b3e7c40f08a8df380ab991895506a5c14fad40d
myintelex <myintelex@163.com>
2017-12-17 20:38:03 +0800

    update the mindmap today

---
+++ updata.bat
@@ -0,0 +1,13 @@
+@echo off
+@title 自动更新脑图
+
+set path=%path%;C:\Program Files\Git\cmd
+
+cd /d D:\A01脑图\0. 知识结构
+
+git add *
+git commit -m "update the mindmap today"
+git push origin master
+
+echo\&echo done...
+pause
