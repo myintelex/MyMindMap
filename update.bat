7e1b537a9aaee645650dd92de1517111b8dcf2cf
myintelex <myintelex@163.com>
2017-12-21 15:59:44 +0800

    update the mindmap today

--- update.bat
+++ update.bat
@@ -5,8 +5,10 @@ set path=%path%;C:\Program Files\Git\cmd
 
 cd /d D:\A01脑图\0. 知识结构
 
+
 git add *
 git commit -m "update the mindmap today"
+git pull origin master
 git push origin master
 
 echo.>D:\1.txt
