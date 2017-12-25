d65ce98823c292df9babe9a8511a20c139f9ea55
myintelex <myintelex@163.com>
2017-12-25 10:46:45 +0800

    update the mindmap today

--- update.bat
+++ update.bat
@@ -5,14 +5,9 @@ set path=%path%;C:\Program Files\Git\cmd
 
 cd /d F:\O-MindMap\MyMindMap
 
-<<<<<<< HEAD
 
-=======
 git pull origin master
->>>>>>> 3847795013f8a17dd9dbb957fefe2b47624f11fd
 git add *
 git commit -m "update the mindmap today"
 git pull origin master
 git push origin master
-
-echo.>D:\1.txt
