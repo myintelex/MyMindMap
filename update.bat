5680b5df1c9e8d9cd624a77fe14dbbe8717626be
myintelex <myintelex@163.com>
2017-12-27 13:55:23 +0800

    update the mindmap today

--- update.bat
+++ update.bat
@@ -9,10 +9,10 @@ echo ----------------start pull-----------------
 
 git pull origin master
 
-echo ----------------pull over -----------------
+echo ----------pull over, add new files---------
 git add *
 git commit -m "update the mindmap today"
 echo ----------------start push-----------------
 git push origin master
-echo ----------------push over -----------------
+echo ----------push over, exit 10s--------------
 ping 127.0.0.1 -n 10 >nul
